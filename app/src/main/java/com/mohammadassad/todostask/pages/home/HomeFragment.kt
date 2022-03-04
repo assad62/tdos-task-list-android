f92be706b32039063dbfd8c455b27850b864fca8
Home User 2 <assad62@hotmail.com>
2022-03-05 01:54:59 +0800

    added bottom nav, and title bar

--- app/src/main/java/com/mohammadassad/todostask/pages/home/HomeFragment.kt
+++ app/src/main/java/com/mohammadassad/todostask/pages/home/HomeFragment.kt
@@ -1,14 +1,21 @@
 package com.mohammadassad.todostask.pages.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mohammadassad.todostask.R
 import com.mohammadassad.todostask.databinding.FragmentHomeBinding
 import com.mohammadassad.todostask.databinding.FragmentLoginBinding
+import com.mohammadassad.todostask.pages.home.calendar.CalendarFragment
+import com.mohammadassad.todostask.pages.home.today.TodayFragment
 import com.mohammadassad.todostask.pages.login.LoginViewModel
 import com.mohammadassad.todostask.storage.LocalStorage
 
@@ -24,12 +31,42 @@ class HomeFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
 
-        (activity as AppCompatActivity).supportActionBar?.show()
+        //(activity as AppCompatActivity).supportActionBar?.show()
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        _binding?.title?.text = "Hello ${LocalStorage.getStringValue("auth_token")}"
+
+        val bottomNavigationView = _binding?.bottomNavigatinView
+        setCurrentFragment(TodayFragment())
+
+
+        bottomNavigationView?.setupWithNavController(findNavController())
+        bottomNavigationView?.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.todayFragment->{
+
+                    setCurrentFragment(TodayFragment())
+                }
+                R.id.calendarFragment->{
+
+                    setCurrentFragment(CalendarFragment())
+                }
+
+
+            }
+            true
+        }
+
+
 
         return binding.root
     }
+
+    private fun setCurrentFragment(fragment:Fragment)=
+        childFragmentManager.beginTransaction().apply {
+            replace(R.id.nav_fragment,fragment)
+            commit()
+        }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
